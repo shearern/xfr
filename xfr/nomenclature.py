0e7406da56ceb6a366aa9ab7e921ddba13c621e4
nshearer@ewu.edu <nshearer@ewu.edu>
2023-12-20 17:05:18 -0800

    Starting ide for sfr scheme

---
+++ xfr/nomenclature.py
@@ -0,0 +1,10 @@
+
+
+
+class XrfPath:
+    """
+    Specifies the path to a file or directory in the Repository.
+
+    Example format:
+        xrf://{project_uuid}/{project_path}?branch=feature/new-feature&path=footage/main.mp4
+    """
